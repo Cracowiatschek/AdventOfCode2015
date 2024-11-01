bed76fe792f26e4da10821125496e72bd7f8d110
Cracowiatschek <piotrbaran0123@gmail.com>
2024-11-01 18:40:13 +0100

    14th Day completed

--- Day_14/solution.py
+++ Day_14/solution.py
@@ -29,8 +29,34 @@ def travel_calculator(raindeer, test_time):
 travel_time = 2503
 result = map(lambda i, j=travel_time: travel_calculator(read_data(data)[i],j), read_data(data))
 
-sp("The best raindeer lead at: ", max(result))
+
+sp("The best reindeer lead at: ", max(result))
+
 
 def raindeer_raid(raindeer, test_time):
+    leaderboard = {i: {"points": 0, "travel": 0, "stamina": raindeer[i]["stamina"],
+                       "speed": raindeer[i]["speed"], "rest":  raindeer[i]["rest"]} for i in raindeer}
+
     for i in range(test_time):
-        for j in raindeer:
+        actual_time = i+1
+        for j in leaderboard:
+            raid_cycle = leaderboard[j]["stamina"]+leaderboard[j]["rest"]
+            last_cycle = actual_time % raid_cycle
+
+            if last_cycle <= leaderboard[j]["stamina"] and actual_time <= raid_cycle:
+                leaderboard[j]["travel"] += leaderboard[j]["speed"]
+            elif last_cycle < leaderboard[j]["stamina"] and actual_time > raid_cycle:
+                leaderboard[j]["travel"] += leaderboard[j]["speed"]
+            else:
+                leaderboard[j]["travel"] += 0
+        get_leader_travel = max(leaderboard[i]["travel"] for i in leaderboard)
+        get_leader = [i for i in leaderboard if leaderboard[i]["travel"] == get_leader_travel]
+
+        for j in get_leader:
+            leaderboard[j]["points"] += 1
+
+    get_winner = max([leaderboard[i]["points"] for i in leaderboard])
+    return get_winner+1
+
+
+sp("The best reindeer has: ", raindeer_raid(read_data(data), travel_time))
